cf89b118fbaf0af63b9bbdee6476d5cb0d58f806
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2023-06-07 13:03:24 -0500

    create login page and public and private routes

---
+++ src/Presentation/pages/login/Login.tsx
@@ -0,0 +1,44 @@
+import "./styles.css";
+import LoginImg from "../../assets/Login.svg";
+
+export const Login = () => {
+  return (
+    <div className="body">
+      <div className="login-container">
+        <div className="login-left">
+          <div className="login-header">
+            <h1>Bienvenido a nuestra aplicación</h1>
+            <p>Por favor inicia sesión</p>
+          </div>
+          <form className="login-form">
+            <div className="login-form-content">
+              <div className="form-item">
+                <label htmlFor="email">Ingrese su correo electronico</label>
+                <input type="text" id="email" />
+              </div>
+              <div className="form-item">
+                <label htmlFor="password">Ingrese su contraseña</label>
+                <input type="password" id="password" />
+              </div>
+              <div className="form-item">
+                <div className="checkbox">
+                  <input type="checkbox" id="rememberMe" checked />
+                  <label htmlFor="rememberMe" className="checkboxLabel">
+                    Guardar usuario
+                  </label>
+                </div>
+              </div>
+              <button type="submit" className="form-login-button">
+                Iniciar sesión
+              </button>
+            </div>
+            <div className="login-form-footer"></div>
+          </form>
+        </div>
+        <div className="login-right">
+          <img src={LoginImg} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,42 +1,13 @@
-import { Home } from "../Presentation/pages/home/Home";
-import { Routes, Route } from "react-router-dom";
-import { MainHome } from "../Presentation/pages/home/components/Main";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Dispatch } from "redux";
-import { selectedNavItem } from "../store/actions/navbarActions";
-import { Category } from "../Presentation/pages/category/Category";
-import { Inventory } from "../Presentation/pages/inventory/Inventory";
-import { Users } from "../Presentation/pages/users/Users";
-import { Roles } from "../Presentation/pages/roles/Roles";
-import { CreateInventory } from "../Presentation/pages/inventory/CreateInventory";
+import { Route, Routes } from "react-router-dom";
+import { DashboardRouter } from "./DashboardRouter";
+import { PublicRouter } from "./publicRouter";
 
 export const AppRouter = () => {
-  const dispatch = useDispatch<Dispatch<any>>(); // eslint-disable-line
-
-  useEffect(() => {
-    const currentUrl = window.location.pathname;
-    dispatch(selectedNavItem(currentUrl));
-  }, [dispatch]);
-
   return (
-    <>
-      <Home />
-      <Routes>
-        <Route path="/" element={<MainHome title={"Dashboard"} />} />
-        <Route path="orders" element={<MainHome title={"Ordenes"} />} />
-        <Route path="customers" element={<MainHome title={"Clientes"} />} />
-        <Route path="inventory" element={<Inventory />} />
-        <Route path="inventory/create-product" element={<CreateInventory />} />
-        <Route
-          path="inventory/edit-product/:id"
-          element={<CreateInventory />}
-        />
-        <Route path="inventory/product/:id" element={<CreateInventory />} />
-        <Route path="category" element={<Category />} />
-        <Route path="users" element={<Users />} />
-        <Route path="role" element={<Roles />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/private/*" element={<DashboardRouter />} />
+      <Route path="/*" element={<PublicRouter />} />
+    </Routes>
   );
 };
